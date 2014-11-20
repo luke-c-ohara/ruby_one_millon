197b9644ea91f97110fb1916417b27e2f0198edf
luke-ohara <luke.c.ohara@gmail.com>
2014-11-20 11:26:52 +0000

    added 30 to words hash and if statement to puts words for 20-30

--- one-million/main.rb
+++ one-million/main.rb
@@ -1,7 +1,14 @@
 require 'pry-byebug'
 
-words_hash = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",8=>"eight",9=>"nine", 10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen", 18=>"eighteen",19=>"nineteen", 20=>"twenty"}
+words_hash = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",8=>"eight",9=>"nine", 10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen", 18=>"eighteen",19=>"nineteen", 20=>"twenty", 30=>"thirty"}
 
-(1..20).each do |number|
- puts words_hash[number]
+(1..30).each do |num|
+  if num <= 20
+    puts words_hash[num]
+  else
+    split = num.to_s.split('').map { |digit| digit.to_i }
+    first = split.first * 10
+    second = split.last 
+    puts words_hash[first] + words_hash[second]
+  end
 end   
